d3273ab1b58e865baa1942b5a3ddfa70db7f2d48
Roman Kozak <cossack.roman@gmail.com>
2024-11-12 21:11:57 +0200

    perfect work

--- context2app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-app_path="/home/roman/selinux-policies/app1/app1.sh"
-
-# Встановлюємо контекст для програми
-sudo semanage fcontext -a -t secure_app_exec_t "$app_path"
-sudo restorecon -v $app_path
--- context2dir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-secure_folder="/home/roman/selinux-policies/app1/safe1"
-# Встановлюємо контекст для папки
-sudo chcon -R -t secure_app_data_t $secure_folder
-sudo semanage fcontext -a -t secure_app_data_t "$secure_folder(/.*)?"
-sudo restorecon -R -v $secure_folder
-
